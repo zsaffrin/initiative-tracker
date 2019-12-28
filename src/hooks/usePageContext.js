1e81e701772773b57d8ae73e601b8e320ad17f3a
Zach Saffrin <zach@zachsaffrin.com>
2019-12-28 16:03:10 -0500

    Remove old utilities

--- src/hooks/usePageContext.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-
-import { PageContext } from '../contexts';
-
-export function usePageContext() {
-  const { activePage, changePage, pageArgs } = useContext(PageContext);
-
-  return { activePage, changePage, pageArgs };
-}
