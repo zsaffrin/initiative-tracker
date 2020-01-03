37f63899c853403ac2a3fc5e3229b8faabffce05
Zach Saffrin <zach@zachsaffrin.com>
2020-01-02 21:35:47 -0500

    Create addParticipant function

--- src/contexts/listContext.js
+++ src/contexts/listContext.js
@@ -1,15 +1,45 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext } from 'react';
 import {
   arrayOf, oneOfType, node, shape,
 } from 'prop-types';
 
+import { generateId } from '../utils';
+import { FirebaseContext } from './firebaseContext';
+
 const ListContext = createContext();
 
-const ListProvider = ({ list, children }) => (
-  <ListContext.Provider value={list}>
-    {children}
-  </ListContext.Provider>
-);
+const ListProvider = ({ list, children }) => {
+  const firebase = useContext(FirebaseContext);
+
+  const updateList = (listId, newData) => {
+    firebase.updateDoc(`lists/${listId}`, newData);
+  };
+
+  const addParticipant = () => {
+    const { id, participants, ...rest } = list;
+    updateList(id, {
+      ...rest,
+      participants: [
+        ...participants,
+        {
+          id: generateId(10),
+          name: '',
+        },
+      ],
+    });
+  };
+
+  return (
+    <ListContext.Provider value={{
+      list,
+      updateList,
+      addParticipant,
+    }}
+    >
+      {children}
+    </ListContext.Provider>
+  );
+};
 ListProvider.propTypes = {
   children: oneOfType([arrayOf(node), node]),
   list: shape({}),
