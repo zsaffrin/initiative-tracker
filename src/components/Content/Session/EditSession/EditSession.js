0adc83570ba292019b622ead3b8d4942c584b3ea
Zach Saffrin <zach@zachsaffrin.com>
2020-01-03 23:02:17 -0500

    Adjust PageLayout, include ManageTurn component

--- src/components/Content/Session/EditSession/EditSession.js
+++ src/components/Content/Session/EditSession/EditSession.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import { Redirect, useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { useListContext } from '../../../../hooks';
 import { Button, ButtonRow, H } from '../../../ui';
+import ManageTurn from './ManageTurn';
 import ParticipantList from './ParticipantList';
+
+const PageLayout = styled.div(({ theme }) => {
+  const { space } = theme;
+  return `
+    display: grid;
+    grid-gap: ${space.md};
+  `;
+});
 
 const EditSession = () => {
   const { list } = useListContext();
   const { sharekey } = useParams();
   const history = useHistory();
+  const { access } = list;
 
-  if (list.access !== 'edit') {
+  if (access !== 'edit') {
     return <Redirect to={`/session/${sharekey}`} />;
   }
 
   return (
-    <div>
-      <H l={2} centered>Manage Session</H>
-      <ParticipantList />
+    <PageLayout>
+      <ManageTurn />
+      <div>
+        <H l={2} centered compact>Manage Participants</H>
+        <ParticipantList />
+      </div>
       <ButtonRow>
         <Button primary onClick={() => history.push(`/session/${sharekey}`)}>Done</Button>
       </ButtonRow>
-    </div>
+    </PageLayout>
   );
 };
 
