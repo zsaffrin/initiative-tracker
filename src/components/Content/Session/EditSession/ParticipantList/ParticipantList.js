07c28ae49ae2ba60f3348dc6d2f60fcd3041177a
Zach Saffrin <zach@zachsaffrin.com>
2020-01-03 22:05:57 -0500

    Items are li, list should be ul

--- src/components/Content/Session/EditSession/ParticipantList/ParticipantList.js
+++ src/components/Content/Session/EditSession/ParticipantList/ParticipantList.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { useListContext } from '../../../../../hooks';
 import { ButtonRow, Button } from '../../../../ui';
 import ParticipantListItem from './ParticipantListItem';
+
+const StyledList = styled.ul(({ theme }) => {
+  const { space } = theme;
+  return `
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  `;
+});
 
 const ParticipantList = () => {
   const { list, addParticipant, sortParticipantsByKey } = useListContext();
@@ -14,11 +24,11 @@ const ParticipantList = () => {
         <Button primary small onClick={addParticipant}>Add New</Button>
         <Button small onClick={() => sortParticipantsByKey('roll')}>Sort By Roll</Button>
       </ButtonRow>
-      <div>
+      <StyledList>
         {participants.length > 0
           ? participants.map((p) => <ParticipantListItem item={p} key={p.id} />)
           : 'No participants'}
-      </div>
+      </StyledList>
     </div>
   );
 };
