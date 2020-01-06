f45c087802fba146d6706ee85ac6294a7449ec34
Zach Saffrin <zach@zachsaffrin.com>
2020-01-05 22:20:42 -0500

    Style up the loading page

--- src/components/Content/Session/SessionLoading.js
+++ src/components/Content/Session/SessionLoading.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+import { Icon } from '../../ui';
+
+const keyFrameSpin = keyframes`
+  100% { 
+    transform:rotate(360deg); 
+  }
+`;
+
+const PageLayout = styled.div(({ theme }) => {
+  const { color, space } = theme;
+  return `
+    color: ${color.accent};
+    display: grid;
+    align-content: center;
+    grid-gap: ${space.xl};
+    justify-items: center;
+  `;
+});
+const SpinningIcon = styled.div`
+  font-size: 2em;
+  animation: ${keyFrameSpin} 2s ease-in-out 0s infinite;
+`;
 
 const SessionLoading = () => (
-  <div>
+  <PageLayout>
+    <SpinningIcon>
+      <Icon name="dice-d20" />
+    </SpinningIcon>
+    <div>
       Loading Session...
-  </div>
+    </div>
+  </PageLayout>
 );
 
 export default SessionLoading;
